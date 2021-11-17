79a9eb186fd63227ac87f6cddfb076b82f5a6c2d
nmd974 <jha.payet@gmail.com>
2021-11-17 15:37:57 +0400

    Suppression de l'option d'ajout auto du +/- si pas de chiffres saisis

--- calculatrice/MainWindow.xaml.cs
+++ calculatrice/MainWindow.xaml.cs
@@ -23,25 +23,23 @@ namespace calculatrice
         public string operation_to_execute = null;
         public bool decimal_activated = false;
         public bool reset_after_equal = false;
+        public bool negate_number = false;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //Mettre le blocage si division par 0
         private void Calculate()
         {
-            
             foreach (string number in this.dataa)
             {
-                Debug.WriteLine(number);
-                string[] split_element = number.Split(".");
+                string[] split_element = number.Split(",");
                 if(split_element.Count() > 1)
                 {
                     this.decimal_activated = true;
                 }
             }
-
-
             if (this.operation_to_execute == "/")
             {
                 if (this.decimal_activated)
@@ -50,7 +48,7 @@ namespace calculatrice
                 }
                 else
                 {
-                    Debug.WriteLine("Modulo" + Int64.Parse(this.dataa[0]) % Int64.Parse(this.dataa[1]));
+                    //Debug.WriteLine("Modulo" + Int64.Parse(this.dataa[0]) % Int64.Parse(this.dataa[1]));
                     if(Int64.Parse(this.dataa[0]) % Int64.Parse(this.dataa[1]) > 0)
                     {
                         this.decimal_activated = true;
@@ -98,18 +96,26 @@ namespace calculatrice
 
             this.operation_to_execute = null;
         }
+
+        //Le retour en arriere enleve juste le 1er char et le ce enleve toute la saisie en cours
+        //Gerer le 0 devant les chiffres
+        //Gerer le +/-
+        //Ajouter la possibilité d'executer des calculs si on a la licence ==> inscription sur appli et générer une clef que l'on save sur l'outil de l'utilisateur
+        //Ajouter un menu qui apparaît quand on appuie sur alt
+
         private void Render_Screen(string context = "")
         {
-            foreach(string test in this.historic)
+            if (this.reset_after_equal)
             {
-                Debug.WriteLine(test);
+                screen_display_historic.Text = "";
+                this.reset_after_equal = false;
             }
-            
+
             if (this.historic.Count() > 2)
             {
-
                 Calculate();
-                if(context == "equal")
+                this.dataa.Clear();
+                if (context == "equal")
                 {
                     this.reset_after_equal = true;
                     if (this.decimal_activated)
@@ -128,8 +134,6 @@ namespace calculatrice
                 else
                 {
                     this.historic.Clear();
-                    this.dataa.Clear();
-
                     if (this.decimal_activated)
                     {
                         screen_display.Text = this.data_decimal.ToString();
@@ -144,61 +148,115 @@ namespace calculatrice
                     }
                 }
             }
-            if (this.reset_after_equal == true)
-            {
-                screen_display_historic.Text = "";
-                this.reset_after_equal = false;
-            }
+
             foreach (string element in this.historic)
             {
                 string tmp = screen_display_historic.Text;
                 screen_display_historic.Text = tmp + element;
             }
+            foreach (string test in this.dataa)
+            {
+                Debug.WriteLine(test);
+            }
+            Debug.WriteLine("END");
         }
+
+        /// <summary>
+        /// Ajoute les chiffres et +/- saisis par l'utilisateur
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Add_next(object sender, RoutedEventArgs e)
         {
+            //On ajoute le contenu des chiffres sur lequel on clique si c'est 0 en tout début alors on ne fait rien
             var element_pressed = (Button)sender;
-            string tmp = screen_display.Text;
-            screen_display.Text = tmp + element_pressed.Content;
+            if(element_pressed.Content.ToString() == "0" && screen_display.Text == "")
+            {
+
+            }
+            else
+            {
+                string tmp = screen_display.Text;
+                screen_display.Text = tmp + element_pressed.Content;
+
+                //Si l'utilisateur a cliqué sur +/- avant d'écrire son numbre alors on ajoute le moins juste après l'ajout de son 1er chiffre
+                //if (this.negate_number && screen_display.Text != "")
+                //{
+                //    Add_before();
+                //}
+            }
+
         }
-        private void Add_before(object sender, RoutedEventArgs e)
+        private void Plus_Moins(object sender, RoutedEventArgs e)
         {
-            var element_pressed = (Button)sender;
-            string tmp = screen_display.Text;
-            screen_display.Text = element_pressed.Content + tmp;
+            Add_before();
+        }
+        /// <summary>
+        /// Zone commentaires
+        /// 
+        /// </summary>
+        private void Add_before()
+        {
+            if (screen_display.Text != "")
+            {
+                string tmp = screen_display.Text;
+                if (this.negate_number && tmp.Contains("-"))
+                {
+                    screen_display.Text = tmp.Substring(1, tmp.Length);
+                    this.negate_number = false;
+                }
+                else
+                {
+                    screen_display.Text = "-" + tmp;
+                    this.negate_number = true;
+                }
+            }
+            //else
+            //{
+            //    //Ici on gère si oui ou non le chiffre est négatif
+            //    this.negate_number = !this.negate_number;
+            //}
         }
 
         private void Operators_click(object sender, RoutedEventArgs e)
         {
             var element_pressed = (Button)sender;
-            Debug.WriteLine(this.reset_after_equal);
-            if (this.reset_after_equal == true)
+            //Verifier s'il y a déjà eu une valeur ajoutée sinon on ne fait rien
+            if(screen_display.Text != "")
             {
-                this.historic.Clear();
-            }
-            this.historic.Add(screen_display.Text);
-            this.historic.Add(element_pressed.Content.ToString());
-            this.dataa.Add(screen_display.Text);
-            if (element_pressed.Content.ToString() == "=")
-            {
-                Render_Screen("equal");
-            }
-            else
-            {
-                if (this.operation_to_execute == null)
+                //Si l'utilisateur a cliqué sur = alors on reset l'historique pour n'afficher que le résultat
+                if (this.reset_after_equal)
                 {
-                    this.operation_to_execute = element_pressed.Content.ToString();
-                    Render_Screen();
+                    this.historic.Clear();
                 }
                 else
                 {
-                    Render_Screen();
-                    this.operation_to_execute = element_pressed.Content.ToString();
+                    this.dataa.Add(screen_display.Text);
                 }
-                Clear_Screen();
-            }
-                       
+                this.historic.Add(screen_display.Text);
+                this.historic.Add(element_pressed.Content.ToString());
+                if (element_pressed.Content.ToString() == "=")
+                {
+                    Render_Screen("equal");
+                }
+                else
+                {
+                    if (this.operation_to_execute == null)
+                    {
+                        this.operation_to_execute = element_pressed.Content.ToString();
+                        Render_Screen();
+                    }
+                    else
+                    {
+                        Render_Screen();
+                        this.operation_to_execute = element_pressed.Content.ToString();
+                    }
+                    Clear_Screen();
+                }
+            }           
         }
+
+        //Gestion du si c'est 0 et qu'on appuie sur le point alors on prend en comtpe
 
         private void Equal_Click(object sender, RoutedEventArgs e)
         {
@@ -211,6 +269,7 @@ namespace calculatrice
         {
             Clear_All_Screen();
             this.decimal_activated = false;
+            this.operation_to_execute = null;
             this.dataa.Clear();
             this.historic.Clear();
             this.data_int = 0;
@@ -219,7 +278,26 @@ namespace calculatrice
 
         private void Erase_content_char(object sender, RoutedEventArgs e)
         {
-            screen_display.Text = screen_display.Text.Substring(0, screen_display.Text.Length);
+            string tmp = screen_display.Text;
+            if(tmp != "")
+            {
+                int limit = tmp.Length - 1;
+                screen_display.Text = tmp.Substring(0, limit);
+            }
+        }
+
+        private void Erase_content_CE(object sender, RoutedEventArgs e)
+        {
+            if (this.reset_after_equal == true)
+            {
+                Clear_All_Screen();
+            }
+            else
+            {
+                Clear_Screen();
+            }
+            
+            
         }
 
         private void Clear_Screen()
